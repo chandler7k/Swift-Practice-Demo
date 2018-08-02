4b8cefa5e8939a9e06353af4eb89e03520840932
chandler7k <chandler7k@hotmail.com>
2018-08-02 22:45:07 +0800

    upload

---
+++ 12-CoreDataFetch/Bubble Tea Finder/Bubble Tea Finder/FilterViewController.swift
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import CoreData
+class FilterViewController: UITableViewController {
+
+  @IBOutlet weak var firstPriceCategoryLabel: UILabel!
+  @IBOutlet weak var secondPriceCategoryLabel: UILabel!
+  @IBOutlet weak var thirdPriceCategoryLabel: UILabel!
+  @IBOutlet weak var numDealsLabel: UILabel!
+
+  // MARK: - Price section
+  @IBOutlet weak var cheapVenueCell: UITableViewCell!
+  @IBOutlet weak var moderateVenueCell: UITableViewCell!
+  @IBOutlet weak var expensiveVenueCell: UITableViewCell!
+
+  // MARK: - Most popular section
+  @IBOutlet weak var offeringDealCell: UITableViewCell!
+  @IBOutlet weak var walkingDistanceCell: UITableViewCell!
+  @IBOutlet weak var userTipsCell: UITableViewCell!
+  
+  // MARK: - Sort section
+  @IBOutlet weak var nameAZSortCell: UITableViewCell!
+  @IBOutlet weak var nameZASortCell: UITableViewCell!
+  @IBOutlet weak var distanceSortCell: UITableViewCell!
+  @IBOutlet weak var priceSortCell: UITableViewCell!
+  
+  //MARK: - Properties
+  var coreDataStack: CoreDataStack!
+  lazy var cheapVenuePredicate: NSPredicate = {
+    return NSPredicate(format: "%K = %@", #keyPath(Venue.priceInfo.priceCategory),"$")
+  }()
+  
+  lazy var moderateVenuePredicate = {
+    return NSPredicate(format: "%K = %@", #keyPath(Venue.priceInfo.priceCategory), "$$")
+  }()
+  lazy var expensiveVenuePredicate = {
+    return NSPredicate(format: "%K = %@", #keyPath(Venue.priceInfo.priceCategory), "$$$")
+  }()
+  
+  
+  // MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    populateCheapVenueCountLabel()
+    populateModerateVenueCountLabel()
+    populateExpensiveVenueCountLabel()
+  }
+}
+
+// MARK: - IBActions
+extension FilterViewController {
+
+  @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+
+  }
+}
+
+// MARK - UITableViewDelegate
+extension FilterViewController {
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+  }
+}
+
+//MARK: - Helper methods
+extension FilterViewController{
+  func populateCheapVenueCountLabel(){
+    let fetchRequest = NSFetchRequest<NSNumber>(entityName: "Venue")
+    fetchRequest.resultType = .countResultType
+    fetchRequest.predicate = cheapVenuePredicate
+    
+    do{
+      let countResult = try coreDataStack.managedContext.fetch(fetchRequest)
+      let count = countResult.first!.intValue
+      firstPriceCategoryLabel.text = "\(count) bubble tea places"
+    }catch let error as NSError{
+      print("Count not fetch \(error), \(error.userInfo)")
+    }
+  }
+  
+  func populateModerateVenueCountLabel(){
+    let fetchRequest = NSFetchRequest<NSNumber>(entityName:"Venue")
+    fetchRequest.resultType = .countResultType
+    fetchRequest.predicate = moderateVenuePredicate
+    
+    do{
+      let countResult = try coreDataStack.managedContext.fetch(fetchRequest)
+      let count = countResult.first!.intValue
+      secondPriceCategoryLabel.text = "\(count) bubble tea places"
+    }catch let error as NSError{
+      print("Count not fetch \(error), \(error.userInfo)")
+    }
+  }
+  
+  func populateExpensiveVenueCountLabel(){
+    let fetchRequest:NSFetchRequest<Venue> = Venue.fetchRequest()
+    fetchRequest.predicate = expensiveVenuePredicate
+    
+    do{
+      let count = try coreDataStack.managedContext.count(for: fetchRequest)
+      thirdPriceCategoryLabel.text = "\(count) bubble tea places"
+    }catch let error as NSError{
+      print("Count not fetch \(error), \(error.userInfo)")
+    }
+    
+    
+  }
+}
---
+++ 12-CoreDataFetch/Bubble Tea Finder/Bubble Tea Finder/ViewController.swift
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import CoreData
+class ViewController: UIViewController {
+
+  // MARK: - Properties
+  private let filterViewControllerSegueIdentifier = "toFilterViewController"
+  fileprivate let venueCellIdentifier = "VenueCell"
+
+  var coreDataStack: CoreDataStack!
+  var fetchRequest: NSFetchRequest<Venue>?
+  var venues:[Venue] = []
+  
+
+  // MARK: - IBOutlets
+  @IBOutlet weak var tableView: UITableView!
+
+  // MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    guard let model = coreDataStack.managedContext.persistentStoreCoordinator?.managedObjectModel, let fetchRequest = model.fetchRequestTemplate(forName: "FetchRequest") as? NSFetchRequest<Venue> else{
+      return
+    }
+    
+    self.fetchRequest = fetchRequest
+    fetchAndReload()
+  }
+
+  // MARK: - Navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    guard segue.identifier == filterViewControllerSegueIdentifier,let navController = segue.destination as? UINavigationController, let filterVC = navController.topViewController as? FilterViewController else{
+      return
+    }
+    filterVC.coreDataStack = coreDataStack
+  }
+}
+
+// MARK: - IBActions
+extension ViewController {
+
+  @IBAction func unwindToVenueListViewController(_ segue: UIStoryboardSegue) {
+  }
+}
+
+// MARK: - Helper
+extension ViewController{
+  func fetchAndReload(){
+    guard let fetchRequest = fetchRequest else{
+      return
+    }
+    
+    do{
+      venues = try coreDataStack.managedContext.fetch(fetchRequest)
+      
+      tableView.reloadData()
+    }catch let error as NSError{
+      print("Could not fetch \(error), \(error.userInfo)")
+    }
+  }
+}
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return venues.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: venueCellIdentifier, for: indexPath)
+    let venue = venues[indexPath.row]
+    
+    cell.textLabel?.text = venue.name
+    cell.detailTextLabel?.text = venue.priceInfo?.priceCategory
+    return cell
+  }
+}
